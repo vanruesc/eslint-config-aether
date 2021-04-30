8cd73d1daf8984c71c0867de2cbae3b6200e307c
Raoul v. R <vanruesc@users.noreply.github.com>
2021-04-30 15:59:59 +0200

    Specify naming-convention

--- typescript.js
+++ typescript.js
@@ -11,11 +11,42 @@ module.exports = {
 	"rules": {
 
 		"indent": "off",
+		"camelcase": "off",
 		"no-unused-vars": "off",
 
 		"@typescript-eslint/indent": ["error", "tab", {
 			"SwitchCase": 1
 		}],
+
+		"@typescript-eslint/naming-convention": ["error",
+			{
+				"selector": "default",
+				"format": ["camelCase"]
+			},
+			{
+				"selector": "variable",
+				"format": ["camelCase", "UPPER_CASE"]
+			},
+			{
+				"selector": "parameter",
+				"format": ["camelCase"],
+				"leadingUnderscore": "allow"
+			},
+			{
+				"selector": "memberLike",
+				"modifiers": ["private"],
+				"format": ["camelCase"],
+				"leadingUnderscore": "forbid"
+			},
+			{
+				"selector": ["objectLiteralProperty", "objectLiteralMethod"],
+				"format": null
+			},
+			{
+				"selector": "typeLike",
+				"format": ["PascalCase"]
+			}
+		],
 
 		"@typescript-eslint/no-empty-function": "off",
 		"@typescript-eslint/no-unused-vars": ["warn", {
