9c889edcdd17bbdcb8ab7f2d9d1df5f31209748a
Raoul v. R <vanruesc@users.noreply.github.com>
2022-05-29 16:20:08 +0200

    Add strict-boolean-expressions

--- typescript-basic.js
+++ typescript-basic.js
@@ -14,6 +14,7 @@ module.exports = {
 		"indent": "off",
 		"no-undef": "off",
 		"no-unused-vars": "off",
+		"strict-boolean-expressions": "error",
 		"@typescript-eslint/indent": ["error", "tab", {
 			"SwitchCase": 1
 		}],
